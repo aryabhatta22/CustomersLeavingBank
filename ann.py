fef825ddc0f861e6c27207f42cf21715397d1bd4
aryabhatta <tarun.hive@hotmail.com>
2020-01-03 22:41:50 +0530

    ANN model trained

--- ann.py
+++ ann.py
@@ -14,12 +14,25 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
-X = dataset.iloc[:, [2, 3]].values
-y = dataset.iloc[:, 4].values
+X = dataset.iloc[:, 3:13 ].values    #independent variable
+y = dataset.iloc[:, 13].values       #dependent variale
 
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])              # X_1 is for country
+
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])              # X_2 is for male/female
+
+
+onehotencoder = OneHotEncoder(categorical_features = [1])       #Craetes dummy variable for country
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
