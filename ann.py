f84562b7cc2fbc89c080aedb78f0b3477a35e8a3
aryabhatta <tarun.hive@hotmail.com>
2020-01-03 22:56:57 +0530

    tested with accuracy of 0.8435

--- ann.py
+++ ann.py
@@ -5,46 +5,58 @@ Created on Tue Dec 31 15:54:28 2019
 @author: tarun
 """
 
-# ---------------------- Part 1: Data preprocessing ---------------------- 
+# ---------------------- Data preprocessing ---------------------- 
 
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
-X = dataset.iloc[:, 3:13 ].values    #independent variable
-y = dataset.iloc[:, 13].values       #dependent variale
+X = dataset.iloc[:, 3:13 ].values    
+y = dataset.iloc[:, 13].values
 
-# Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-
 labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])              # X_1 is for country
 
 labelencoder_X_2 = LabelEncoder()
 X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])              # X_2 is for male/female
 
-
-onehotencoder = OneHotEncoder(categorical_features = [1])       #Craetes dummy variable for country
+onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
-# Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
+
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
-# Fitting classifier to the Training set
-# Create your classifier here
 
-# Predicting the Test set results
+# ---------------------- ANN ---------------------- 
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+                    # Intializing the ANN
+classifier = Sequential()                   
+    #   Hidden Layer 1
+classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11 ))     
+    # Hidden Layer 2                                                                                                
+classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
+    # Output Layer
+classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid', input_dim = 11 ))
+
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'] )
+    
+classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
+                                                                         
+# ---------------------- Making prediction & evaluating model---------------------- 
+                                                                         
 y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
 
-# Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
+cm = confusion_matrix(y_test, y_pred)               # accuracy  = (1556 +131 ) /2000 = 0.8435
